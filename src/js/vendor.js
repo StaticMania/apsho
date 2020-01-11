e250850c0e680573ab66efdca76f1f8da2fae87a
monirulislamriaj <riaj1132@gmail.com>
2020-01-11 12:26:42 +0600

    test commit

---
+++ src/js/vendor.js
@@ -0,0 +1,8 @@
+@import 'C:/Users/RAJ/Desktop/obie/src/plugins/jQuery/jquery.min.js';
+@import 'C:/Users/RAJ/Desktop/obie/src/plugins/bootstrap/bootstrap.min.js';
+@import 'C:/Users/RAJ/Desktop/obie/src/plugins/slick/slick.min.js';
+@import 'C:/Users/RAJ/Desktop/obie/src/plugins/aos/aos.js';
+@import 'C:/Users/RAJ/Desktop/obie/src/plugins/headroomjs/headroom.min.js';
+@import 'C:/Users/RAJ/Desktop/obie/src/plugins/match-height/match-height.js';
+@import 'C:/Users/RAJ/Desktop/obie/src/plugins/gsap/TweenMax.min.js';
+@import 'C:/Users/RAJ/Desktop/obie/src/plugins/gsap/TimelineMax.min.js';
